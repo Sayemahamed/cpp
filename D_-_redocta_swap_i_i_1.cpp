14344612be9245289f6f191a57432ac169c0c00b
Sayem <sayemahamed183@gmail.com>
2022-08-15 21:21:04 +0600

    ok

--- D_-_redocta_swap_i_i_1.cpp
+++ D_-_redocta_swap_i_i_1.cpp
@@ -1,32 +1,21 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
+map<char, int>value={{'a',1},{'t',2},{'c',3},{'o',4},{'d',5},{'e',6},{'r',7}};
+int main()
+{
   string s;
   cin >> s;
-  
-  map<string,int> mp;
-  queue<string> q;
-  
-  mp[s]=0;
-  q.push(s);
-  
-  while(!q.empty()){
-    string current=q.front();q.pop();
-    if(current=="atcoder"){
-      cout << mp[current] << "\n";
-      return 0;
-    }
-    
-    for(int i=1;i<7;i++){
-      string next=current;
-      swap(next[i-1],next[i]);
-      if(mp.find(next)==mp.end()){
-        q.push(next);
-        mp[next]=mp[current]+1;
+  int ans=0;
+  for (int i=0; i<7; ++i)
+  {
+    for (int j=i+1; j<7; ++j)
+    {
+      if(value[s[i]]>value[s[j]])
+      {
+        swap(s[i], s[j]);
+        ans++;
       }
     }
   }
-  return 0;
+  cout << ans<<endl;
 }
