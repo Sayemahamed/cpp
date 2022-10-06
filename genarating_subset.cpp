e8a8a383f45737bd83e07a56eb582d4ef70ce971
Sayem <sayemahamed183@gmail.com>
2022-10-06 12:47:11 +0600

    ok

--- genarating_subset.cpp
+++ genarating_subset.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector <char> datas;
-void generatingSubset(vector <char> datas,string &s,int position)
+void generatingSubset(vector <char> &datas,string &s,int position)
 {
     if(s.length()==position)
     {
         datas.push_back('\n');
         for(auto &it:datas)cout<<it;
+        datas.clear();
         return;
     }
     generatingSubset(datas,s,position+1);
     datas.push_back(s[position]);
     generatingSubset(datas,s,position+1);
-    datas.pop_back();
+    datas.clear();
 }
 int main()
 {
