44f7d13da5fd27aed8659697d40341f5a17cdff8
Sayem <sayemahamed183@gmail.com>
2022-10-13 20:20:40 +0600

    ok

--- determinete.cpp
+++ determinete.cpp
@@ -1,96 +1,78 @@
-// C++ program to find Determinant of a matrix
-#include <iostream>
-using namespace std;
-
-// Dimension of input square matrix
-#define N 4
-
-// Function to get cofactor of mat[p][q] in temp[][]. n is
-// current dimension of mat[][]
-void getCofactor(int mat[N][N], int temp[N][N], int p,
-				int q, int n)
-{
-	int i = 0, j = 0;
-
-	// Looping for each element of the matrix
-	for (int row = 0; row < n; row++)
-	{
-		for (int col = 0; col < n; col++)
-		{
-			// Copying into temporary matrix only those
-			// element which are not in given row and
-			// column
-			if (row != p && col != q)
-			{
-				temp[i][j++] = mat[row][col];
-
-				// Row is filled, so increase row index and
-				// reset col index
-				if (j == n - 1)
-				{
-					j = 0;
-					i++;
-				}
-			}
-		}
-	}
-}
-
-/* Recursive function for finding determinant of matrix.
-n is current dimension of mat[][]. */
-int determinantOfMatrix(int mat[N][N], int n)
-{
-	int D = 0; // Initialize result
-
-	// Base case : if matrix contains single element
-	if (n == 1)
-		return mat[0][0];
-
-	int temp[N][N]; // To store cofactors
-
-	int sign = 1; // To store sign multiplier
-
-	// Iterate for each element of first row
-	for (int f = 0; f < n; f++)
-	{
-		// Getting Cofactor of mat[0][f]
-		getCofactor(mat, temp, 0, f, n);
-		D += sign * mat[0][f]
-			* determinantOfMatrix(temp, n - 1);
-
-		// terms are to be added with alternate sign
-		sign = -sign;
-	}
-
-	return D;
-}
-
-/* function for displaying the matrix */
-void display(int mat[N][N], int row, int col)
-{
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < col; j++)
-			cout <<" " << mat[i][j];
-		cout <<"n";
-	}
-}
-
-// Driver program to test above functions
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+ 
+#define   SIZE   10
+ 
+ 
 int main()
 {
-	/* int mat[N][N] = {{6, 1, 1},
-					{4, -2, 5},
-					{2, 8, 7}}; */
-
-	int mat[N][N] = { { 1, 0, 0, 0 },
-					{ 3, 0, 0, 5 },
-					{ 2, 9, 8, -3 },
-					{ 1, 0, 5, 0 } };
-
-	// Function call
-	cout <<"Determinant of the matrix is : " << determinantOfMatrix(mat, N);
-	return 0;
+     float a[SIZE][SIZE], x[SIZE], ratio, det=1;
+     int i,j,k,n;
+ 
+     /* Inputs */
+     /* 1. Reading number of unknowns */
+     printf("Enter Order of Matrix: ");
+     scanf("%d", &n);
+ 
+     /* 2. Reading Matrix */
+     printf("\nEnter Coefficients of Matrix: \n");
+     for(i=0;i< n;i++)
+     {
+          for(j=0;j< n;j++)
+          {
+               printf("a[%d][%d]=",i,j);
+               scanf("%f", &a[i][j]);
+          }
+     }
+ 
+    /* Here we are using Gauss Elimination
+    Technique for transforming matrix to
+    upper triangular matrix */
+    /* Applying Gauss Elimination */
+     for(i=0;i< n;i++)
+     {
+          if(a[i][i] == 0.0)
+          {
+               printf("Mathematical Error!");
+               exit(0);
+          }
+          for(j=i+1;j< n;j++)
+          {
+               ratio = a[j][i]/a[i][i];
+ 
+               for(k=0;k< n;k++)
+               {
+                    a[j][k] = a[j][k] - ratio*a[i][k];
+               }
+          }
+     }
+ 
+     /* Displaying upper triangular matrix */
+      
+     /* Not required, just for the sake of understanding */
+      
+     /* By analyzing upper triangular matrix you 
+     will get what's going on :) */
+     printf("\nUpper Triangular Matrix: \n");
+     for(i=0;i< n;i++)
+     {
+          for(j=0;j< n;j++)
+          {
+               printf("%0.2f\t",a[i][j]);
+          }
+          printf("\n");
+     }
+ 
+     /* Finding determinant by multiplying
+     elements in principal diagonal elements */
+     for(i=0;i< n;i++)
+     {
+         det = det * a[i][i];
+     }
+ 
+     printf("\n\nDeterminant of given matrix is: %0.3f", det);
+ 
+ 
+     return 0;
 }
-
-// this code is contributed by shivanisinghss2110
