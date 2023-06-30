2bf86ebf418baac451e2f0bc7fd812fcd1b79b80
Sayem <sayemahamed183@gmail.com>
2023-06-30 21:13:51 +0600

    ok

--- C_The_Tag_Game.cpp
+++ C_The_Tag_Game.cpp
@@ -1,30 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int>tree[200005];
-int parent[200005];
-int depth[200005];
-int dp[200005];
-void dfs(int cur){
-	dp[cur] = depth[cur];
-	for(int nxt:tree[cur]){
-		if(nxt==parent[cur]) continue;
-		parent[nxt] = cur;
-		depth[nxt] = depth[cur]+1;
-		dfs(nxt);
-		dp[cur] = max(dp[cur],dp[nxt]);
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+long long diff, Bob;
+//----------------------------------------------------------------//
+// helper functions //
+void dfs( vector<long long>list[], vector<long long>& ans, vector<long long>& parent, long long position, long long distance ) {
+	if (position == Bob and !ans[ position ])diff = distance;
+	ans[ position ] = distance;
+	for (auto& child : list[ position ]) {
+		parent[ child ] = position;
+		if (!ans[ child ])
+			dfs( list, ans, parent, child, distance + 1 );
+		ans[ position ] = max( distance, ans[ child ] );
 	}
 }
-int main(){
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	int n,x; cin>>n>>x;
-	for(int i=1; i<n; i++){
-		int a,b; cin>>a>>b;
-		tree[a].emplace_back(b);
-		tree[b].emplace_back(a);
+
+//----------------------------------------------------------------//
+// solve function//
+void solve()
+{
+	long long siz, x, y;
+	cin >> siz >> Bob;
+	vector<long long>list[ siz + 1 ], ans( siz + 1, 0 ), parent( siz + 1, 0 );
+	for (long long i = 1;i < siz;i++) {
+		cin >> x >> y;
+		list[ x ].push_back( y );
+		list[ y ].push_back( x );
 	}
-	depth[1] = 1; dfs(1);
-	int diff = depth[x]-1; 
-	diff = diff/2 - (diff%2?0:1);
-	for(int i=0; i<diff; i++) x = parent[x];
-	cout<<(dp[x] - 1)*2;
+	dfs( list, ans, parent, 1, 1 );
+	diff++;
+	diff /= 2;
+	while (diff--)Bob = parent[ Bob ];
+	cout << (ans[ Bob ] - 1) * 2 << endl;
+}
+
+
+//----------------------------------------------------------------//
+// main function//
+int main()
+{
+	ios_base::sync_with_stdio( false );
+	cin.tie( NULL );
+	cout.tie( NULL );
+	long long test = 1;
+	// cin >> test;
+	while (test--)
+	{
+		solve();
+	}
 }
