3937763ffbabca1e959d4d217018300dcb3c4bd9
Sayem <sayemahamed183@gmail.com>
2023-07-06 00:28:40 +0600

    ok

--- C_Tenzing_and_Balls.cpp
+++ C_Tenzing_and_Balls.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 //----------------------------------------------------------------//
 // definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
 #define endl '\n'
 //----------------------------------------------------------------//
 // preDefined functions//
@@ -11,44 +13,41 @@ using namespace std;
 
 //----------------------------------------------------------------//
 // helper functions //
-
+void dp( vector<long long>& colors, map<long long, vector<long long>>& index, vector<long long>& memoise, long long i ) {
+	if (i - 1) dp( colors, index, memoise, i - 1 );
+	long long ans ;
+	for (auto& it : index[ colors[ i ] ]) {
+		if (it >= i)break;
+		ans = i - it + 1;
+		memoise[ i ] = max( ans + memoise[ it - 1 ], memoise[ i ] );
+	}
+	if (!memoise[ i ])memoise[ i ] = memoise[ i - 1 ];
+}
 
 //----------------------------------------------------------------//
 // solve function//
-void solve()
-{
-    long long siz;
-    cin>>siz;
-    vector<long long>dat(siz);
-    set<long long>indicator[siz+1];
-    for(long long i=0;i<siz;i++){
-        cin>>dat[i];
-        indicator[dat[i]].insert(i);
-    }
-    for(long long i=0;i<siz;i++){
-        if(dat[i] and (*indicator[dat[i]].lower_bound(i)!= *indicator[dat[i]].rbegin())){
-            long long end=*indicator[dat[i]].rbegin();
-            for(long long j=*indicator[dat[i]].lower_bound(i);j<=end;j++){
-                dat[j]=0;
-            }
-        }
-    }
-    long long ans=0;
-    for(auto&it:dat)if(it==0)ans++;
-    cout<<ans<<endl;
+void solve() {
+	long long siz;cin >> siz;
+	vector<long long>colors( siz + 1, 0 ), memoise( siz + 1, 0 );
+	map<long long, vector<long long>>index;
+	for (long long i = 1;i <= siz;i++) {
+		cin >> colors[ i ];
+		index[ colors[ i ] ].push_back( i );
+	}
+	dp( colors, index, memoise, siz );
+	auto ans = max_element( memoise.begin(), memoise.end() );
+	cout << *ans << endl;
 }
 
 //----------------------------------------------------------------//
 // main function//
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    long long test = 1;
-    cin >> test;
-    while (test--)
-    {
-        solve();
-    }
+int main() {
+	ios_base::sync_with_stdio( false );
+	cin.tie( NULL );
+	cout.tie( NULL );
+	long long test = 1;
+	cin >> test;
+	while (test--) {
+		solve();
+	}
 }
