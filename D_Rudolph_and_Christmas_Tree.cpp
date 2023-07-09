0c46478586016e11c60db6c44835656d1607ba7b
Sayem <sayemahamed183@gmail.com>
2023-07-09 18:03:25 +0600

    ok

--- D_Rudolph_and_Christmas_Tree.cpp
+++ D_Rudolph_and_Christmas_Tree.cpp
@@ -18,20 +18,25 @@ using namespace std;
 //----------------------------------------------------------------//
 // solve function//
 void solve() {
-    long long numberOfBranches,base,height;
-    cin>>numberOfBranches>>base>>height;
-    vector<long long>branchHeights(numberOfBranches);
-    for(auto&it:branchHeights)cin>>it;
-    double ans=0;
-    for(long long i=0;i<numberOfBranches;i++) 
+    double numberOfBranches, base, height;
+    cin >> numberOfBranches >> base >> height;
+    vector<double>branchHeights( numberOfBranches );
+    for (auto& it : branchHeights)cin >> it;
+    double area = height * base * 0.5;
+    double ans = area * numberOfBranches;
+    for (long long i = 0;i < numberOfBranches - 1;i++) {
+        double ratio = 1 - (min( branchHeights[ i + 1 ] - branchHeights[ i ], height )) / height;
+        ans -= area * ratio * ratio;
+    }
+    cout << fixed << setprecision( 7 ) << ans << endl;
 }
 
 //----------------------------------------------------------------//
 // main function//
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
     long long test = 1;
     cin >> test;
     while (test--) {
