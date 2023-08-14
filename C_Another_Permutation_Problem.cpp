d9c5fb360378b641ec56f1911583998714408478
Sayem <sayemahamed183@gmail.com>
2023-08-14 23:05:50 +0600

    ok

--- C_Another_Permutation_Problem.cpp
+++ C_Another_Permutation_Problem.cpp
@@ -1,56 +1,55 @@
-#include <iostream>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-void solve() {
-	int N = 0; cin >> N;
-	int ans = 0;
-	vector<int> pr;
-	pr.assign(N * N, -1);
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j <= N; ++j) {
-			pr[i * j - 1] = 1;
-		}
-	}
-	for (int mx = N * N; mx >= 1; --mx) {
-		if (pr[mx - 1] == -1) continue;
-		vector<vector<int>> a;
-		int curans = -mx;
-		bool br = false;
-		a.assign(N, vector<int>());
-		for (int j = N; j >= 1; --j) {
-			int num = min(mx / j, N);
-			if (num < 1) {
-				br = true;
-				break;
-			}
-			a[num - 1].push_back(j);
-		}
-		if (br) break;
-		stack<int> s;
-		for (int i = 0; i < N; ++i) {
-			s.push(i + 1);
-			bool brk = false;
-			for (auto x : a[i]) {
-				if (s.empty()) {
-					brk = true; break;
-				}
-				curans += s.top() * x;
-				s.pop();
-			}
-			if (brk) break;
-		}
-		ans = max(ans, curans);
-	}
-	cout << ans << "\n";
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+long long getSum( vector<long long>& a, vector<long long>& b ) {
+    long long sum = 0, mx = 0, tmp;
+    for (long long i = 0; i < a.size(); ++i) {
+        tmp = a[ i ] * b[ i ];
+        sum += tmp;
+        mx = max( mx, tmp );
+    }
+    return sum - mx;
 }
- 
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    long long number, ans = 2;
+    cin >> number;
+    vector<long long>a( number + 1 ), b( number + 1 );
+    for (long long i = 0;i <= number;i++) {
+        a[ i ] = i;
+        b[ i ] = i;
+    }
+    for (long long i = 2;i < number;i++) {
+        reverse( b.begin() + i, b.end() );
+        ans = max( ans, getSum( a, b ) );
+        reverse( b.begin() + i, b.end() );
+    }
+    cout << ans << endl;
+}
+
+//----------------------------------------------------------------//
+// main function//
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int t = 0; cin >> t;
-	while (t--) solve();
-	return 0;
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
+    long long test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
 }
