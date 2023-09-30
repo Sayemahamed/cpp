5eab06968632cf6e0a419b690f91a2f14abb18e6
Sayem <sayemahamed183@gmail.com>
2023-09-30 20:45:49 +0600

    ok

---
+++ A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    long long maxLimit, tools, current;cin >> maxLimit >> current >> tools;
+    long long ans = min( maxLimit, current );
+    while (tools--) {
+        cin >> current;
+        ans += min( maxLimit - 1, current );
+    }
+    cout << ans << endl;
+}
+
+//----------------------------------------------------------------//
+// main function//
+int main() {
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
+    long long test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+}
