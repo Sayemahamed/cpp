a8554912939d48d72acb7ee4d1dd9dd1a1f54b10
Sayem <sayemahamed183@gmail.com>
2023-10-06 10:18:37 +0600

    ok

--- B_Same_Parity_Summands.cpp
+++ B_Same_Parity_Summands.cpp
@@ -19,6 +19,21 @@ using namespace std;
 // solve function//
 void solve() {
     long long totalSum, numbers;cin >> totalSum >> numbers;
+    int isPossible = 0;
+    if ((numbers - 1) * 2 < totalSum and (totalSum - ((numbers - 1) * 2)) % 2 == 0)isPossible = 2;
+    else if (numbers - 1 < totalSum and (totalSum - (numbers - 1)) % 2 == 1)
+        isPossible = 1;
+    if (isPossible == 0)cout << "NO" << endl;
+    else if (isPossible == 1) {
+        cout << "YES" << endl;
+        for (long long i = 1;i < numbers;i++)cout << 1 << ' ';
+        cout << totalSum - numbers + 1 << endl;
+    }
+    else if (isPossible == 2) {
+        cout << "YES" << endl;
+        for (long long i = 1;i < numbers;i++)cout << 2 << ' ';
+        cout << totalSum - (numbers - 1) * 2 << endl;
+    }
 }
 
 //----------------------------------------------------------------//
