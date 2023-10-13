247a1e3ce56ac4a7881ec084e939e8e4c5eeb638
Sayem <sayemahamed183@gmail.com>
2023-10-13 11:03:31 +0600

    ok

--- C_Perfect_Square.cpp
+++ C_Perfect_Square.cpp
@@ -18,29 +18,15 @@ using namespace std;
 //----------------------------------------------------------------//
 // solve function//
 void solve() {
-    long long siz, ans = 0;cin >> siz;
-    vector<vector<char>>dat( siz, vector<char>( siz ) ), dat2( siz, vector<char>( siz ) );
+    long long siz, ans = 0, diff = 0;cin >> siz;
+    vector<vector<char>>dat( siz, vector<char>( siz ) );
     for (auto& it : dat)for (auto& c : it)cin >> c;
-    for (long long i = 0;i < siz;i++) {
-        for (long long j = 0;j < siz;j++) {
-            dat2[ i ][ j ] = dat[ siz - i - 1 ][ j ];
+    for (long long i = 0;i < siz / 2;i++) {
+        for (long long j = 0;j < siz / 2;j++) {
+            char mx = max( { dat[ i ][ j ],dat[ siz - j - 1 ][ i ],dat[ siz - i - 1 ][ siz - j - 1 ],dat[ j ][ siz - i - 1 ] } );
+            ans += mx * 4 - (dat[ i ][ j ] + dat[ siz - j - 1 ][ i ] + dat[ siz - i - 1 ][ siz - j - 1 ] + dat[ j ][ siz - i - 1 ]);
         }
     }
-    for (long long i = 0;i < siz;i++) {
-        for (long long j = 0;j < siz;j++) {
-            ans += abs( dat[ i ][ j ] - dat2[ i ][ j ] );
-            dat[ i ][ j ] = max( dat[ i ][ j ], dat2[ i ][ j ] );
-            dat2[ i ][ j ] = dat[ i ][ j ];
-        }
-    }
-    for (long long i = 0;i < siz;i++) {
-        for (long long j = 0;j < siz;j++) {
-            ans += abs( dat[ i ][ j ] - dat2[ i ][ j ] );
-            dat[ i ][ j ] = max( dat[ i ][ j ], dat2[ i ][ j ] );
-            dat2[ i ][ j ] = dat[ i ][ j ];
-        }
-    }
-
     cout << ans << endl;
 }
 
