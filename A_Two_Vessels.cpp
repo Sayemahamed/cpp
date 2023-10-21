38af270dc53c7e832745b007b5f2114a4d8da086
Sayem <sayemahamed183@gmail.com>
2023-10-21 19:58:50 +0600

    ok

---
+++ A_Two_Vessels.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    long long potA, potB, potC;
+    cin >> potA >> potB >> potC;
+    cout << (((abs( potA - potB ) + potC - 1) / potC) + 1) / 2 << endl;
+}
+
+//----------------------------------------------------------------//
+// main function//
+int main() {
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
+    long long test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+}
