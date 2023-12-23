b54c99ccf37db6bfd6d310835dd7988dadb6d281
Sayem <sayemahamed183@gmail.com>
2023-12-23 19:42:53 +0600

    ok

---
+++ B_Trouble_Sort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    long long size, x;cin >> size;
+    bool oneExist = false, zeroExist = false;
+    for (long long i = 0;i < size;i++) {
+        cin >> x;
+    }
+    for (long long i = 0;i < size;i++) {
+        cin >> x;
+        if(x)oneExist=true;
+        else zeroExist=true;
+    }
+    if(oneExist and zeroExist)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+//----------------------------------------------------------------//
+// main function//
+int main() {
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
+    long long test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+}
