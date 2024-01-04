2d1c877d0c649ecd83f08d164a9c46448b35ed60
Sayem <sayemahamed183@gmail.com>
2024-01-04 13:36:51 +0600

    ok

--- B_1_Palindrome_Game_easy_version.cpp
+++ B_1_Palindrome_Game_easy_version.cpp
@@ -13,32 +13,12 @@ using namespace std;
 
 //----------------------------------------------------------------//
 // helper functions //
-bool isSorted( vector<long long >& data ) {
-    for (long long i = 0;i < data.size() - 1;i++) {
-        if (data[ i ] > data[ i + 1 ])return false;
-    }
-    return true;
-}
+
 
 //----------------------------------------------------------------//
 // solve function//
 void solve() {
-    long long siz, mn = INT64_MAX;cin >> siz;
-    vector<long long>data( siz );
-    for (auto& it : data)cin >> it, mn = min( mn, it );
-    if (siz & 1) {
-        cout << "YES" << endl;
-        return;
-    }
-    long long x = data[ 0 ] + 1 - mn;
-    data[ 0 ] -= x;data[ 1 ] -= x;
-    for (long long i = 1;i < siz - 1;i++) {
-        x = data[ i - 1 ] - data[ i ];
-        data[ i ] += x;
-        data[ i + 1 ] += x;
-    }
-    if (isSorted( data ))cout << "YES" << endl;
-    else cout << "NO" << endl;
+    cout << "BOB" << endl;
 }
 
 //----------------------------------------------------------------//
