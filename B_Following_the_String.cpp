d85c8823701bbd0fb494751d3e05c50e293b7801
Sayemahamed <sayemahamed183@gmail.com>
2024-02-13 00:49:50 +0600

    ok

--- B_Following_the_String.cpp
+++ B_Following_the_String.cpp
@@ -18,14 +18,30 @@ using namespace std;
 //----------------------------------------------------------------//
 // solve function//
 void solve() {
+    long long size;cin >> size;
+    map<long long, long long>data;
+    for (long long i = 0;i < size;i++) {
+        data[ i ];
+    }
+    for (long long i = 0;i < size;i++) {
+        long long x;cin >> x;
+        for (auto& it : data) {
+            if (it.second == x) {
+                cout << (char)('a' + it.first);
+                data[ it.first ]++;
+                break;
+            }
+        }
+    }
+    cout << endl;
 }
 
 //----------------------------------------------------------------//
 // main function//
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio( false );
+    cin.tie( NULL );
+    cout.tie( NULL );
     long long test = 1;
     cin >> test;
     while (test--) {
