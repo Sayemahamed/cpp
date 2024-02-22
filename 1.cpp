7131b76e5662bd70c76cae678b2eca523cc3152a
Sayemahamed <sayemahamed183@gmail.com>
2024-02-22 18:47:19 +0600

    ok

--- 1.cpp
+++ 1.cpp
@@ -1,34 +1,37 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<stack>
 using namespace std;
 int main()
 {
-    map<long long,long long>Ans;
+    map<long long, long long>Ans;
     stack<long long>waiting_room;
     vector<long long>data;
     long long t;
-    cin>>t;
+    cin >> t;
     for (long long i = 0; i < t; ++i)
     {
         long long int x;
-        cin>>x;
-        data.push_back(x);
+        cin >> x;
+        data.push_back( x );
     }
-    for(long long i = 0; i < t; ++i)
+    for (long long i = 0; i < t; ++i)
     {
-        while(!waiting_room.empty()&&data[i]>data[waiting_room.top()])
+        while (!waiting_room.empty() && data[ i ] > data[ waiting_room.top() ])
         {
-            Ans[waiting_room.top()]=data[i];
+            Ans[ waiting_room.top() ] = data[ i ];
             waiting_room.pop();
         }
-        waiting_room.push(i);
+        waiting_room.push( i );
     }
-    while(waiting_room.size())
+    while (waiting_room.size())
     {
-        Ans[waiting_room.top()]=-1;
+        Ans[ waiting_room.top() ] = -1;
         waiting_room.pop();
     }
-    for(auto it:Ans)
+    for (auto it : Ans)
     {
-        cout<<data[it.first]<<" "<<it.second<<endl;
+        cout << data[ it.first ] << " " << it.second << endl;
     }
 }
---
+++ C_Jatayu_s_Balanced_Bracket_Sequence.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    int n;cin>>n;
+    
+}
+
+//----------------------------------------------------------------//
+// main function//
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+}
