f2f0c3ffde290d586e8728526d3c2c570c8eb6ad
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-05-01 22:36:52 +0600

    ok

---
+++ C_Drazil_and_Factorial.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------//
+// definitions //
+#define all(x)  x.begin(), x.end()
+#define rall(x)  x.rbegin(), x.rend()
+#define endl '\n'
+//----------------------------------------------------------------//
+// preDefined functions//
+number
+//----------------------------------------------------------------//
+//data types//
+
+//----------------------------------------------------------------//
+// helper functions //
+
+
+//----------------------------------------------------------------//
+// solve function//
+void solve() {
+    long long numberSize;cin>>numberSize;
+}
+
+//----------------------------------------------------------------//
+// main function//
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test = 1;
+    // cin >> test;
+    while (test--) {
+        solve();
+    }
+}
