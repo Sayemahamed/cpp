eaa9fb7238ee8bf06fe11db72eb56de6762fc41d
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-06-22 19:21:36 +0600

    changed to linux compiler

---
+++ symbolTable.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i = 0, j = 0, x = 0, n, flag = 0;
+    void* p, * add[ 15 ];
+    char cdh, srch, b[ 15 ], c;
+    cout << "Expression terminated by $: " << endl;
+    while (c = getchar() != '$')
+        b[ i++ ] = c;
+    n=i-1;
+    cout<<"Given expression is: "<<b<<endl;
+}
